2da8b938e796f9483caf6eb41fa4d5b693c88cfe
Krishna Patil <128321437+krishnapatil6446@users.noreply.github.com>
2025-10-15 20:37:19 +0530

    arrays mid

--- Basics/arrays.java
+++ Basics/arrays.java
@@ -8,12 +8,36 @@ public class arrays {
     //OR int[] intArray; DECLARATION
     // age = new int[5] // ALLOCATION
 
-    int age[]= new int [5];
-    age [0] = 2;
-    age [1] = 5;
-    System.out.println(age[0]);
-    System.out.println(age[1]);
-    System.out.println(age[2]);
+    // int age[]= new int [5];
+    // age [0] = 2;
+    // age [1] = 5;
+    // System.out.println(age[0]);
+    // System.out.println(age[1]);
+    // System.out.println(age[2]);
+
+    // int marks[] = {11,23,43,54,29};
+    // System.out.println(marks[4]);
+    
+    // String names[]= {"Raj", "Krishna", "Raju", "Ram", "Karan"};
+    // for (int i = 0; i< names.length;i++){
+    //     System.out.println("Name is : "+ names[i]);
+    // }    
+
+    // // FOR EACH LOOP
+
+    // for(String name: names){
+    //     System.out.println("For each : " + name);
+    // }
+
+    int numbers [] = {12,1,22,34,31,52,89,8};
+    int sum = 0;
+    for (int number: numbers){
+        sum+=number;
+    }
+    for (int i=0; i<numbers.length ;i++){
+        
+    }
+     System.out.println("the value is : " + sum);
 
 }
 }
